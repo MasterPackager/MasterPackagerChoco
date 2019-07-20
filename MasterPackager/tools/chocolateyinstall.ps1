b36fc448778a2c7fb8a55ecec958f09c1221f8f2
MasterPackager <info@masterpackager.com>
2019-07-20 12:30:26 +0300

    Master Packager 19.4.1

--- MasterPackager/tools/chocolateyinstall.ps1
+++ MasterPackager/tools/chocolateyinstall.ps1
@@ -1,14 +1,14 @@
 ﻿$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://www.masterpackager.com/installer/standard/Master_Packager_Std_19.3.1.0.msi'
+$url        = 'https://www.masterpackager.com/installer/standard/Master_Packager_Std_19.4.1.0.msi'
 $packageName = 'masterpackager'
-$chocolateyPackageVersion = '19.3.1'
+$chocolateyPackageVersion = '19.4.1'
 
 $packageArgs = @{
   packageName   = $packageName
   chocolateyPackageVersion = $chocolateyPackageVersion
   fileType      = 'MSI'
   url           = $url
-  checksum      = '3A14914EEAE7F758DD3A2C3D7B47DA0876A3CB6CD9868286427C36EA99FB22CC'
+  checksum      = '19C17949F54F67FC39AB000A36B0EA1CDDA617ECA75F93933A2B1F5717EC373A'
   checksumType	= 'sha256'
   softwareName  = 'MasterPackager*'
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($chocolateyPackageVersion).MsiInstall.log`""
